247e16258f3424cdcf633065ea9f4e225c0c21f8
Maciej Mierzyński <Maciej@MacBook-Pro.local>
2023-03-23 10:47:19 +0100

    Add content and styling in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,29 @@
 import Navbar from "../components/Navbar"
+import image_home from "../image_home.png"
 
 const Home = () => {
     return(
-        <div className="overlay">
+        <>
         <Navbar/>
+        <div className="overlay">
+            <div id="title_home">
+                <span id="who_title">WHO</span><span id="rides_title">rides</span>
+            </div>
+            <div id="sign_and_image">
+                <div className="columns">
+                    <p className="columnTxt">Leave your headphones,</p>
+                    <p className="columnTxt" id="bottomTxt">you are not going alone today</p>
+                    <button className="button_curved"><span>SIGN UP</span></button>
+                </div>
+                <div className="columns">
+                    <img src={image_home} />
+                </div>
+            </div>
+            <div id="bottom_home">
+                <div className="bg_rectangle"></div>
+            </div>
         </div>
+        </>
     )
 }
 export default Home
