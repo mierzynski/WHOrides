dbd09959df0404065839b8021b8a5dbb5c31263d
Maciej Mierzyński <Maciej@MacBook-Pro.local>
2023-03-23 21:59:06 +0100

    Added content and styling in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,11 +16,20 @@ const Home = () => {
                     <button className="button_curved"><span>SIGN UP</span></button>
                 </div>
                 <div className="columns">
-                    <img src={image_home} />
+                    <img id="img_home" src={image_home} />
                 </div>
             </div>
             <div id="bottom_home">
-                <div className="bg_rectangle"></div>
+                <div className="bg_rectangle">
+                    <div id="bottom_title">What our app actually do?</div>
+                    <div id="bottom_content">Our application will help you find a driving partner, which will have similar preferences to yours.
+                        Unlike various group on social networks, dating apps, where were you looking for companionship to drive.
+                        Here to goal is clear: <span>spin for kilometers</span></div>
+                    <div id="bottom_buttons">
+                        <button className="button_curved" id="botton_SIGNUP">SIGN UP</button>
+                        <button className="button_curved" id="botton_DMUS">DM US</button>
+                    </div>
+                </div>
             </div>
         </div>
         </>
