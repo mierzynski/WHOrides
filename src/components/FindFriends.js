8540eebb68ee56dd422e3024947159b64077349e
Maciej Mierzyński <mmierzynski@icloud.com>
2023-03-30 22:02:16 +0200

    Add filters and list of users

--- src/components/FindFriends.js
+++ src/components/FindFriends.js
@@ -1,18 +1,94 @@
-
+import RowUser from "./RowUser";
+import { FixedSizeList as List } from "react-window";
 
 const FindFriends = () => {
+  return (
+    <div className="findFriends_container">
+      <div className="column">
+        <div className="filters bg_rectangle">
+          {/* first column with clickable buttons */}
+          <div className="columnFilters">
+            <div className="titleOfFilter">bike types</div>
+            <div className="clicableTypes">
+              <button className="clickedButtonType">gravel</button>
+              <button className="notClickedButtonType">road</button>
+              <button className="notClickedButtonType">mtb</button>
+            </div>
+            <div className="titleOfFilter">bike types</div>
+            <div className="clicableTypes">
+              <button className="clickedButtonType">gravel</button>
+              <button className="notClickedButtonType">road</button>
+              <button className="notClickedButtonType">mtb</button>
+            </div>
+          </div>
+          {/* second column with ranges */}
+          <div className="columnFilters">
+            <div className="titleOfFilter">average pace</div>
+            <div className="rangeFilter">
+              <input
+                className="inputRange"
+                type="number"
+                placeholder="22"
+              ></input>
+              <div className="rangeMinus">-</div>
+              <input
+                className="inputRange"
+                type="number"
+                placeholder="22"
+              ></input>
+            </div>
+            <div className="titleOfFilter">distance range</div>
+            <div className="rangeFilter">
+              <input
+                className="inputRange"
+                type="number"
+                placeholder="22"
+              ></input>
+              <div className="rangeMinus">-</div>
+              <input
+                className="inputRange"
+                type="number"
+                placeholder="22"
+              ></input>
+            </div>
+          </div>
+          {/* third column with filters */}
+          <div className="columnFilters">
+            <div className="titleOfFilter">age range</div>
+            <div className="rangeFilter">
+              <input
+                className="inputRange"
+                type="number"
+                placeholder="22"
+              ></input>
+              <div className="rangeMinus">-</div>
+              <input
+                className="inputRange"
+                type="number"
+                placeholder="22"
+              ></input>
+            </div>
+            <div className="titleOfFilter">location</div>
+            <div className="rangeFilter">
+              <input
+                className="inputLocation"
+                type="text"
+                placeholder="Poznań"
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div className="data_list">
+          <List height={400} itemCount={6} itemSize={100} width="100%">
+            {RowUser}
+          </List>
+        </div>
+      </div>
 
-    return(
-        <div className="findFriends_container">
-        <div className="column">
-            <div className="filters bg_rectangle"></div>
-            <div className="data_list"></div>
-        </div>
-
-        <div className="column">
-            <div className="details bg_rectangle"></div>
-        </div>
-        </div>
-    )
-}
-export default FindFriends
+      <div className="column">
+        <div className="details bg_rectangle"></div>
+      </div>
+    </div>
+  );
+};
+export default FindFriends;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,61 +1,55 @@
-import Navbar from "../components/Navbar"
-import NotLoggedContent from "../components/NotLoggedContent"
+import Navbar from "../components/Navbar";
+import NotLoggedContent from "../components/NotLoggedContent";
 import LogInForm from "../components/LogInForm";
 import SignUpForm from "../components/SignUpForm";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Profile from "../components/Profile";
 import FindFriends from "../components/FindFriends";
-import {useCookies} from 'react-cookie'
+import { useCookies } from "react-cookie";
 
 const Home = (props) => {
-    const content = props.content
+  const content = props.content;
 
-    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
 
-    const authToken = cookies.AuthToken
+  const authToken = cookies.AuthToken;
 
-    return(
-        <>
-        <Navbar isLogged={authToken}/>
-        <div className="overlay">
-            {authToken ?
-            (<div id="title_logged">
-            <span id="who_logged">WHO</span><span id="rides_logged">rides</span>
-            </div>)
-            :
-            (<div id="title_home">
-            <span id="who_title">WHO</span><span id="rides_title">rides</span>
-            </div>)}
-            
-            
-            {
-                (() => {
-                    if (content === "login") {
-                        return(<LogInForm/>)
-                    }
-                    else if (content === "signup") {
-                        return(<SignUpForm/>)
-                        }
-                    else if (content === "about") {
-                        return(<About/>)
-                        }
-                    else if (content === "contact") {
-                        return(<Contact/>)
-                        }
-                    else if (content === "profile") {
-                            return(<Profile/>)
-                            }  
-                    else if (content === "find_friends") {
-                            return(<FindFriends/>)
-                            }  
-                    else {
-                        return(<NotLoggedContent/>)
-                        }
-                })()  
-            } 
-        </div>
-        </>
-    )
-}
-export default Home
+  return (
+    <>
+      <Navbar isLogged={authToken} />
+      <div className="overlay">
+        {authToken ? (
+          <div id="title_logged">
+            <span id="who_logged">WHO</span>
+            <span id="rides_logged">rides</span>
+          </div>
+        ) : (
+          <div id="title_home">
+            <span id="who_title">WHO</span>
+            <span id="rides_title">rides</span>
+          </div>
+        )}
+
+        {(() => {
+          if (content === "login") {
+            return <LogInForm />;
+          } else if (content === "signup") {
+            return <SignUpForm />;
+          } else if (content === "about") {
+            return <About />;
+          } else if (content === "contact") {
+            return <Contact />;
+          } else if (content === "profile") {
+            return <Profile />;
+          } else if (content === "find_friends") {
+            return <FindFriends />;
+          } else {
+            return <NotLoggedContent />;
+          }
+        })()}
+      </div>
+    </>
+  );
+};
+export default Home;
