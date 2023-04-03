4f1fd9598ef1f38da950edaca99017d4d96aea3a
Maciej Mierzyński <mmierzynski@icloud.com>
2023-04-03 19:52:02 +0200

    Add styling and comment in profile details

---
+++ src/components/CommentRow.js
@@ -0,0 +1,11 @@
+const CommentRow = () => {
+  return (
+    <div className="commentBg">
+      <div className="commentAuthor">JAREK</div>
+      <div className="starsRate">o o o o o</div>
+      <div className="commentsContent">Polecam tego allegrowicza!</div>
+      <div className="commentDate">01.04.2023</div>
+    </div>
+  );
+};
+export default CommentRow;
--- src/components/FindFriends.js
+++ src/components/FindFriends.js
@@ -1,5 +1,6 @@
 import RowUser from "./RowUser";
 import FiltersBar from "./FiltersBar";
+import CommentRow from "./CommentRow";
 import { FixedSizeList as List } from "react-window";
 
 const FindFriends = () => {
@@ -15,7 +16,31 @@ const FindFriends = () => {
       </div>
 
       <div className="column">
-        <div className="details bg_rectangle"></div>
+        <div className="profile_details bg_rectangle">
+          <div id="photos_profile"></div>
+          <div id="detailsAndComments">
+            <div id="detailsType">
+              <span>name:</span>
+              <span>age:</span>
+              <span>location:</span>
+              <span>avg pace:</span>
+              <span>distance range:</span>
+            </div>
+            <div id="detailsData">
+              <span>Maciej</span>
+              <span>24</span>
+              <span>Poznań</span>
+              <span>18-22km/h</span>
+              <span>30-60km</span>
+            </div>
+            <div id="commentsInProfileDetails">
+              <span>comments</span>
+              <div id="commentsHole">
+                <CommentRow />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
